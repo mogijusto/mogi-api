fab85befa3e679fb837e549e8beb21ebdc381e40
agalvs <61298082+agalvs@users.noreply.github.com>
2020-05-21 23:18:23 -0300

    Entidade de BD recebeu mais campos.

---
+++ mogi-justo/mogi-justo-model/src/main/java/br/com/mogi/justo/model/Desconto.java
@@ -0,0 +1,42 @@
+package br.com.mogi.justo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Desconto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	
+	private String valor;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getValor() {
+		return valor;
+	}
+
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+
+}
