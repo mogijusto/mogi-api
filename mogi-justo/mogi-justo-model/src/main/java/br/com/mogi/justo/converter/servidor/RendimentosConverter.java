368e13960d3f03894e224055cee131981d23e7fd
jowred <32473283+jowred@users.noreply.github.com>
2020-06-18 18:07:34 -0300

    Aprimoramento da classe de conversão

--- mogi-justo/mogi-justo-model/src/main/java/br/com/mogi/justo/converter/servidor/RendimentosConverter.java
+++ mogi-justo/mogi-justo-model/src/main/java/br/com/mogi/justo/converter/servidor/RendimentosConverter.java
@@ -1,6 +1,7 @@
 package br.com.mogi.justo.converter.servidor;
 
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
 
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
@@ -8,6 +9,8 @@ import javax.persistence.Converter;
 @Converter
 public class RendimentosConverter implements AttributeConverter<String, BigDecimal> {
 
+	private static final DecimalFormat BRL = new DecimalFormat("###,###.###");
+	
 	@Override
 	public BigDecimal convertToDatabaseColumn(String rendimentos) {
 		return rendimentos != null ? new BigDecimal(this.convertCurrencyPattern(this.removeThousandSeparator(rendimentos))) : new BigDecimal("0.0");
@@ -15,18 +18,15 @@ public class RendimentosConverter implements AttributeConverter<String, BigDecim
 
 	@Override
 	public String convertToEntityAttribute(BigDecimal rendimentos) {
-		return rendimentos != null ? this.convertCurrencyPattern(rendimentos.toString()) : "";
+		return rendimentos != null ? BRL.format(rendimentos) : "";
 	}
 	
 	public String convertCurrencyPattern(String valor) {
-		if (valor != null) {
-			return valor.contains(",") ? valor.replace(",", ".") : valor.replace(".", ",");
-		}
-		return "";
+		return valor != null ? valor.replace(",", ".") : "";
 	}
 	
 	public String removeThousandSeparator(String valor) {
-		return valor != null ? valor.replace(".", "") : "";
+		return valor != null ? valor.replaceAll("\\.", "") : "";
 	}
 
 }
--- mogi-justo/mogi-justo-model/src/test/java/br/com/mogi/justo/converter/servidor/RendimentosConverterTest.java
+++ mogi-justo/mogi-justo-model/src/test/java/br/com/mogi/justo/converter/servidor/RendimentosConverterTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.math.BigDecimal;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class RendimentosConverterTest {
@@ -13,35 +12,22 @@ public class RendimentosConverterTest {
 	
 	@Test
 	void deve_remover_o_separador_de_milhar_e_converter_o_valor_em_decimal_para_a_tabela() {
-		BigDecimal bd = new BigDecimal("1999.23");
+		BigDecimal esperado = new BigDecimal("1999.23");
 		BigDecimal rendimentos = converter.convertToDatabaseColumn("1.999,23");
-		assertEquals(bd, rendimentos);
-	}
-	
-	@Test
-	void deve_converter_o_valor_em_decimal_para_a_tabela() {
-		BigDecimal bd = new BigDecimal("1999.23");
-		BigDecimal rendimentos = converter.convertToDatabaseColumn("1999,23");
-		assertEquals(bd, rendimentos);
+		assertEquals(esperado, rendimentos);
 	}
 	
 	@Test
 	void deve_converter_o_valor_em_string_para_a_classe() {
 		BigDecimal bd = new BigDecimal("1999.23");
 		String rendimentos = converter.convertToEntityAttribute(bd);
-		assertEquals(bd.toString().replace(".", ","), rendimentos);
+		assertEquals("1.999,23", rendimentos);
 	}
 	
 	@Test
-	void deve_trocar_os_sinais_de_divisao_de_milhar_e_decimal_do_padrao_brasileiro_para_o_americano() {
-		String valor = converter.convertCurrencyPattern("1999,23");
-		assertEquals("1999.23", valor);
-	}
-	
-	@Test
-	void deve_trocar_os_sinais_de_divisao_de_milhar_e_decimal_do_padrao_americano_para_o_brasileiro() {
-		String valor = converter.convertCurrencyPattern("1999.23");
-		assertEquals("1999,23", valor);
+	void deve_trocar_o_decimal_do_padrao_brasileiro_para_o_americano() {
+		String valor = converter.convertCurrencyPattern("19,99");
+		assertEquals("19.99", valor);
 	}
 	
 	@Test
@@ -52,8 +38,8 @@ public class RendimentosConverterTest {
 	
 	@Test
 	void deve_remover_o_separador_de_milhar() {
-		String rendimentos = converter.removeThousandSeparator("1.000,01");
-		assertEquals("1000,01", rendimentos);
+		String rendimentos = converter.removeThousandSeparator("1.000.000,00");
+		assertEquals("1000000,00", rendimentos);
 	}
 	
 	@Test
@@ -61,17 +47,4 @@ public class RendimentosConverterTest {
 		String rendimentos = converter.removeThousandSeparator(null);
 		assertEquals("", rendimentos);
 	}
-	
-	@Test
-	@Disabled
-	void bd() {
-		try {
-			String s = "1.999.23";
-			BigDecimal bd = new BigDecimal(s);
-			System.out.println(bd);
-		} catch (Exception e ) {
-			e.printStackTrace();
-		}
-	}
-
 }
